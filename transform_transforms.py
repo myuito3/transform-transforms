e9e835fbc9d53317764eacab908458cb95721bcf
myuito3 <m.yuito3@gmail.com>
2024-12-03 03:25:09 +0000

    dev

--- transform_transforms.py
+++ transform_transforms.py
@@ -6,7 +6,6 @@ from dataclasses import fields, dataclass, is_dataclass
 from pathlib import Path
 from typing import Optional
 
-import imageio.v3 as imageio
 import numpy as np
 
 
@@ -48,7 +47,7 @@ class FrameParams:
 
 
 @dataclass
-class TransformsJsonParams:
+class TransformsParams:
     w: Optional[int] = None
     h: Optional[int] = None
     fl_x: Optional[float] = None
@@ -65,7 +64,7 @@ class TransformsJsonParams:
     ply_file_path: Optional[str] = None
 
     @classmethod
-    def create(cls, data: dict) -> TransformsJsonParams:
+    def create(cls, data: dict) -> TransformsParams:
         frames = [
             FrameParams(
                 file_path=frame_data["file_path"],
@@ -98,24 +97,63 @@ class TransformsJsonParams:
             ply_file_path=data["ply_file_path"] if "ply_file_path" in data else None,
         )
 
+    def update(
+        self,
+        w=None,
+        h=None,
+        fl_x=None,
+        fl_y=None,
+        cx=None,
+        cy=None,
+        frames=None,
+        k1=None,
+        k2=None,
+        p1=None,
+        p2=None,
+        camera_model=None,
+        applied_transform=None,
+        ply_file_path=None,
+    ):
+        self.w = w if w is not None else self.w
+        self.h = h if h is not None else self.h
+        self.fl_x = fl_x if fl_x is not None else self.fl_x
+        self.fl_y = fl_y if fl_y is not None else self.fl_y
+        self.cx = cx if cx is not None else self.cx
+        self.cy = cy if cy is not None else self.cy
+        self.frames = frames if frames is not None else self.frames
+        self.k1 = k1 if k1 is not None else self.k1
+        self.k2 = k2 if k2 is not None else self.k2
+        self.p1 = p1 if p1 is not None else self.p1
+        self.p2 = p2 if p2 is not None else self.p2
+        self.camera_model = (
+            camera_model if camera_model is not None else self.camera_model
+        )
+        self.applied_transform = (
+            applied_transform
+            if applied_transform is not None
+            else self.applied_transform
+        )
+        self.ply_file_path = (
+            ply_file_path if ply_file_path is not None else self.ply_file_path
+        )
+
 
 class TransformTransforms:
-    transforms_json_params: TransformsJsonParams
+    xs_params: TransformsParams
     image_filenames: list[str]
-    _resize: Optional[tuple[int, int]] = None
-    _crop: Optional[tuple[int, int]] = None
 
     def __init__(self, input_dir: Path) -> None:
         if not isinstance(input_dir, Path):
             input_dir = Path(input_dir)
         assert input_dir.exists(), f"{input_dir} does not exist"
+
         self.input_dir = input_dir
         self._initialize()
 
     def _initialize(self) -> None:
         assert "transforms.json" in os.listdir(self.input_dir)
         data = load_from_json(self.input_dir / "transforms.json")
-        self.transforms_json_params = TransformsJsonParams.create(data)
+        self.xs_params = TransformsParams.create(data)
 
         assert "images" in os.listdir(self.input_dir)
         self.image_filenames = os.listdir(self.input_dir / "images")
@@ -126,42 +164,42 @@ class TransformTransforms:
             for filename in self.image_filenames
             if any(
                 filename == frame.file_path.split("/")[-1]
-                for frame in self.transforms_json_params.frames
+                for frame in self.xs_params.frames
             )
         ]
         associated_frames = [
             frame
-            for frame in self.transforms_json_params.frames
+            for frame in self.xs_params.frames
             if frame.file_path.split("/")[-1] in associated_images
         ]
-        self.transforms_json_params.frames = associated_frames
+
+        self.xs_params.update(frames=associated_frames)
         self.image_filenames = associated_images
 
-    def resize(self, size: tuple[int, int]) -> None:
-        self._resize = size
+    def resize(self, width: int, height: int) -> None:
+        unit_fl_x = self.xs_params.fl_x / self.xs_params.w
+        unit_fl_y = self.xs_params.fl_y / self.xs_params.h
+        new_fl_x = unit_fl_x * width
+        new_fl_y = unit_fl_y * height
 
-    def crop(self, size: tuple[int, int]) -> None:
-        self._crop = size
+        self.xs_params.update(
+            w=width,
+            h=height,
+            fl_x=new_fl_x,
+            fl_y=new_fl_y,
+            cx=width // 2,
+            cy=height // 2,
+        )
 
     def dump(self, output_dir: str) -> None:
         output_dir = Path(output_dir)
-        image_output_dir = output_dir / "images"
-        os.makedirs(image_output_dir, exist_ok=True)
-
-        for filename in self.image_filenames:
-            image = imageio.imread(self.input_dir / "images" / filename)
-            if self._resize is not None:
-                pass
-            if self._crop is not None:
-                pass
-            imageio.imwrite(image_output_dir / filename, image)
-
+        os.makedirs(output_dir, exist_ok=True)
         with open(output_dir / "transforms.json", mode="w", encoding="utf-8") as file:
-            json.dump(dataclass_to_dict(self.transforms_json_params), file, indent=4)
+            json.dump(dataclass_to_dict(self.xs_params), file, indent=4)
 
 
 # pytest
 if __name__ == "__main__":
-    tt = TransformTransforms("data/equ")
+    tt = TransformTransforms("data/rest copy")
     tt.associate_frames()
-    tt.dump(output_dir="data/equ_new")
+    tt.dump(output_dir="data/rest")
